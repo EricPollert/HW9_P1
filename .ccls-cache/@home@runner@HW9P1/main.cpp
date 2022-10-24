ac6aa6f2be3dfeee5ce8ec937e529b538a6a03b8
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-24 03:32:35 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HW9P1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+  
+  const int COL_NUM = 7;
+  const int ROW_NUM = 60;
+  int grade [ROW_NUM][COL_NUM] = {{}
+
+  for (int r = 0; r < NUM_ROWS; r++){
+  for (int c = 0; c < NUM_COLS; c++){
+    cout << setw(5) << myMat[r][c];
+  
+    
+    
+    }
+    cout << endl;
+  }
+  
+}
