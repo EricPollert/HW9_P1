28cf16640b25b0bae5a257ada2b9a6ca3502ecb5
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-25 03:21:00 +0000

    added first unweighted average

--- .ccls-cache/@home@runner@HW9P1/main.cpp
+++ .ccls-cache/@home@runner@HW9P1/main.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 int main() {
   
-  const int COL_NUM = 7;
-  const int ROW_NUM = 60;
-  int grade [ROW_NUM][COL_NUM] = {{}
+  const int COL_NUM = 5;
+  const int ROW_NUM = 5;
+  int grade [ROW_NUM][COL_NUM] = { {100, 100, }};
 
-  for (int r = 0; r < NUM_ROWS; r++){
-  for (int c = 0; c < NUM_COLS; c++){
-    cout << setw(5) << myMat[r][c];
-  
-    
+  for (int r = 0; r < ROW_NUM; r++){
+  for (int c = 0; c < COL_NUM; c++){
+    cout << setw(5) << grade[r][c];
     
     }
+    int avg = (grade[r][1] + grade[r][2] + grade[r][3] + grade[r][4]) / 4;
+    cout << setw(5) << avg;
+    
     cout << endl;
   }
   
