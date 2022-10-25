5bfd1edf8254f4991c8b1a6ff93f619c28ee76a1
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-25 03:34:31 +0000

    Added weighted average and test case values

--- .ccls-cache/@home@runner@HW9P1/main.cpp
+++ .ccls-cache/@home@runner@HW9P1/main.cpp
@@ -7,7 +7,12 @@ int main() {
   
   const int COL_NUM = 5;
   const int ROW_NUM = 5;
-  int grade [ROW_NUM][COL_NUM] = { {100, 100, }};
+  int grade [ROW_NUM][COL_NUM] = {{1, 100, 100, 100, 100},
+                                  {2, 100, 0, 100, 0},
+                                  {3, 82, 94, 73, 86},
+                                  {4, 64, 74, 84, 94},
+                                  {5, 94, 84, 74, 64} };
+  
 
   for (int r = 0; r < ROW_NUM; r++){
   for (int c = 0; c < COL_NUM; c++){
@@ -16,8 +21,12 @@ int main() {
     }
     int avg = (grade[r][1] + grade[r][2] + grade[r][3] + grade[r][4]) / 4;
     cout << setw(5) << avg;
+    int avg1 = (grade[r][1] * 0.2 + grade[r][2] * 0.3 + grade[r][3] * 0.3 + grade[r][4] * 0.2) / 4;
+    cout << setw(5) << avg1;
     
     cout << endl;
   }
   
 }
+
+
