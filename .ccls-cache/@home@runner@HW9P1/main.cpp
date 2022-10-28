65c8a3beff11a6e299530bdaf4a908e141806ddd
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-28 15:36:25 +0000

    added printMatrix function, required that order of parts of the code be changed

--- .ccls-cache/@home@runner@HW9P1/main.cpp
+++ .ccls-cache/@home@runner@HW9P1/main.cpp
@@ -3,6 +3,22 @@
 
 using namespace std;
 
+
+void printMatrix(int grade[][5], const int ROW_NUM, const int COL_NUM){
+  for (int r = 0; r < ROW_NUM; r++){
+  for (int c = 0; c < COL_NUM; c++){
+    cout << setw(5) << grade[r][c];
+    
+    }
+    double avg = (grade[r][1] + grade[r][2] + grade[r][3] + grade[r][4]) / 4;
+    cout << setw(5) << avg;
+    double avg1 = grade[r][1] * 0.2 + grade[r][2] * 0.3 + grade[r][3] * 0.3 + grade[r][4] * 0.2;
+    cout << setw(5) << avg1;
+    
+    cout << endl;
+  }
+  }
+
 int main() {
   
   const int COL_NUM = 5;
@@ -12,21 +28,8 @@ int main() {
                                   {3, 82, 94, 73, 86},
                                   {4, 64, 74, 84, 94},
                                   {5, 94, 84, 74, 64} };
-  
 
-  for (int r = 0; r < ROW_NUM; r++){
-  for (int c = 0; c < COL_NUM; c++){
-    cout << setw(5) << grade[r][c];
-    
-    }
-    int avg = (grade[r][1] + grade[r][2] + grade[r][3] + grade[r][4]) / 4;
-    cout << setw(5) << avg;
-    int avg1 = (grade[r][1] * 0.2 + grade[r][2] * 0.3 + grade[r][3] * 0.3 + grade[r][4] * 0.2) / 4;
-    cout << setw(5) << avg1;
-    
-    cout << endl;
-  }
+  
+printMatrix(grade, ROW_NUM, COL_NUM);
   
 }
-
-
